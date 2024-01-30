8c769ddfa0064767e113efdba422e62141460884
Finn Hackshaw <hackshawf5@gmail.com>
2024-01-31 11:30:13 +1300

    feat: course creation form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,6 +1,11 @@
+import { IconBade } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
+import { ImageForm } from "./_components/image-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -12,6 +17,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
+    },
+  });
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
     },
   });
 
@@ -48,8 +59,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
         <div>
           <div className="flex items-center gap-x-2">
+            <IconBade icon={LayoutDashboard} />
             <h2 className="text-xl">Customise your course</h2>
           </div>
+          <TitleForm initialData={course} courseId={course.id} />
+          <DescriptionForm initialData={course} courseId={course.id} />
+          <ImageForm initialData={course} courseId={course.id} />
         </div>
       </div>
     </div>
--- components/icon-badge.tsx
+++ components/icon-badge.tsx
@@ -11,10 +11,46 @@ const backgroundVariants = cva(
         default: "bg-sky-100",
         success: "bg-emerald-100",
       },
-      iconVariant: {
-        default: "text-sky-700",
-        success: "text-emerald-700",
+      size: {
+        default: "p-2",
+        sm: "p-1",
       },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
     },
   }
 );
+
+const iconVariants = cva("", {
+  variants: {
+    variant: {
+      default: "text-sky-700",
+      success: "text-emerald-700",
+    },
+    size: {
+      default: "h-8 w-8",
+      sm: "h-4 w-4",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+type BackgroundVariantsProps = VariantProps<typeof backgroundVariants>;
+type IconVariantsProps = VariantProps<typeof iconVariants>;
+
+interface IconBadgeProps extends BackgroundVariantsProps, IconVariantsProps {
+  icon: LucideIcon;
+}
+
+export const IconBade = ({ icon: Icon, variant, size }: IconBadgeProps) => {
+  return (
+    <div className={cn(backgroundVariants({ variant, size }))}>
+      <Icon className={cn(iconVariants({ variant, size }))} />
+    </div>
+  );
+};
